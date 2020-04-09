80371c3d64de8790fd7e9722bedb030b020ced5b
arb1g19 <arb1g19@soton.ac.uk>
2020-04-09 17:22:37 +0100

    Finished help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,9 @@
 /**
  * http://usejsdoc.org/
  */
+const Discord = require('discord.js');
 const config = require('../config.json');
+const main = require('../index.js');
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
@@ -9,19 +11,28 @@ module.exports = {
 	usage: '[command name]',
 	cooldown: 3,
 	execute(message, args) {
+		const embed = new Discord.MessageEmbed().setColor(main.getSotonColour());
+		const embed2 = new Discord.MessageEmbed().setColor(main.getSotonColour());
 		const data = [];
-		const { commands } = message.client;
+		const { commands, mentions } = message.client;
 
 		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
+			embed.setTitle('Here\'s a list of all my commands:')
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${config.prefix}help [command name]\` to get info on a specific command!`);
-
-			return message.author.send(data, { split: true })
+			embed.setDescription(data);
+			embed2.setTitle('Here\'s a list of the things I can respond to:')
+			
+			embed2.setDescription(mentions.map(mention => `${mention.name.capitalize()}${mention.aliases ? ", ".concat(mention.aliases.map(alias => alias.capitalize()).join(', ')) : ""}`).join(', '));
+			return message.author.send(embed).then(()=> message.author.send(embed2)
 				.then(() => {
 					if (message.channel.type === 'dm') return;
 					message.reply('I\'ve sent you a DM with all my commands!');
 				})
+				.catch(error => {
+					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+				}))
 				.catch(error => {
 					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
 					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
@@ -33,16 +44,15 @@ module.exports = {
 			if (!command) {
 				return message.reply('that\'s not a valid command!');
 			}
-
-			data.push(`**Name**: ${command.name}`);
+			embed.setTitle(`**Name**: ${command.name}`)
 
 			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 			if (command.description) data.push(`**Description:** ${command.description}`);
 			if (command.usage) data.push(`**Usage:** ${config.prefix}${command.name} ${command.usage}`);
 
 			data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-			message.channel.send(data, { split: true });
+			embed.setDescription(data);
+			message.channel.send(embed);
 		}
 	},
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 /**
  * A ping pong bot, whenever you send "ping", it replies "pong".
  */
-
+String.prototype.capitalize = function() {
+	  return this.charAt(0).toUpperCase() + this.slice(1)
+}
 const fileSystem = require('fs');
 // Import the discord.js module
 const Discord = require('discord.js');
-//Load the config file
+// Load the config file
 const config = require('./config.json');
 // Create an instance of a Discord client
 const client = new Discord.Client();
-//Read the command files in the commands directory
+// Read the command files in the commands directory
 client.commands = new Discord.Collection();
 client.mentions = new Discord.Collection();
 const commandFiles = fileSystem.readdirSync(`${__dirname}/commands`).filter(file => file.endsWith('.js'));
@@ -22,11 +24,11 @@ for (const file of mentionFiles) {
 	const mention = require(`${__dirname}/mentions/${file}`);
 	client.mentions.set(mention.name, mention);
 }
-//Cooldowns
+// Cooldowns
 const cooldowns = new Discord.Collection();
 /**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
+ * The ready event is vital, it means that only _after_ this will your bot start
+ * reacting to information received from Discord
  */
 client.on('ready', () => {
 	console.log('I am ready!');
@@ -36,35 +38,37 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.guild && config.included_guidlds && !config.included_guidlds.find(guildid => message.guild.id == guildid)) return;
 	if (message.guild && config.excluded_guidlds && config.excluded_guidlds.find(guildid => message.guild.id == guildid)) return;
-	//if (message.author == client.users.fetch("JamesTheSheep#8509")){
-	//	message.reply("Baaaaa");
-	//}
+	// if (message.author == client.users.fetch("JamesTheSheep#8509")){
+	// message.reply("Baaaaa");
+	// }
 	
-	if (message.mentions.users.first() === client.user) {
-		const lowerCaseMessage = message.content.toLowerCase();
-		responsesUsed = client.mentions.filter(mention => {
-			if (lowerCaseMessage.includes(mention.name)) return true;
-			if (mention.aliases == null) return false;
-			for (const alias of mention.aliases){
-				if (lowerCaseMessage.includes(alias)) return true;
-			}
-			return false;
-		});
-		if (responsesUsed.size > 0){
-			try {
-				return responsesUsed.first().execute(message);
-			} catch (error) {
-				console.error(error);
+	if (message.mentions.users.first() === client.user || (!message.guild && message.author != client.user)) {
+		const args = message.content.split(/ +/);
+		// Get rid of mention
+		if (message.mentions.users.first() === client.user) args.shift();
+		if (!args[0].startsWith(config.prefix)) {
+			const lowerCaseMessage = message.content.toLowerCase();
+			responsesUsed = client.mentions.filter(mention => {
+				if (lowerCaseMessage.includes(mention.name)) return true;
+				if (mention.aliases == null) return false;
+				for (const alias of mention.aliases){
+					if (lowerCaseMessage.includes(alias)) return true;
+				}
+				return false;
+			});
+			if (responsesUsed.size > 0){
+				try {
+					return responsesUsed.first().execute(message);
+				} catch (error) {
+					console.error(error);
+				}
 			}
 		}
 		
-		
-		const args = message.content.split(/ +/);
-		//Get rid of mention
-		args.shift();
-		if (!args[0].startsWith(config.prefix)) return message.channel.send("Baaa, I didn't Quite Catch that");//Is a command?
+
+		if (!args[0].startsWith(config.prefix)) return message.channel.send("Baaa, I didn't Quite Catch that");// Is a command?
 		const commandName = args[0].slice(config.prefix.length).toLowerCase();
-		args.shift();//Get rid of name
+		args.shift();// Get rid of name
 		const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 		if (!command) return message.reply('Baaa, I couldn\'t find that command!');
 		if (!cooldown(command, message.author)) return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
@@ -83,10 +87,11 @@ client.on('message', message => {
 		}
 	}
 });
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
+// Log our bot in using the token from
+// https://discordapp.com/developers/applications/me
 client.login(config.token);
 var currentSotonColour = false;
-function getSotonColour(){
+module.exports.getSotonColour = function(){
 	if (currentSotonColour){
 		currentSotonColour = false;
 		return 0xfecb00;
